17fa662a26c2e3a5405dba96db5e239027b5c5fc
geronimoc17 <gerocepeda@gmail.com>
2023-06-29 01:42:56 -0300

    actualizacion en login y home

--- componentes/navBar.php
+++ componentes/navBar.php
@@ -4,7 +4,7 @@
     <ul>
       <li><a href="../views/buscarNoticia.php"><img class="icon-small" src="../images/news.png"/></img></a></li></span>
       <li><a href="../views/buscar.php"><img class="icon-small" src="../images/lupa.png"/></img></a></li></span>
-			<li><a href="cargar.php"><img class="icon-small" src="../images/cargar.png"/></img></a></li></span>
+			<li><a href="cargar.php"><img class="icon-small" src="../images/cargar.png" width="40px"/></img></a></li></span>
     </ul>
 
 </nav>
--- views/home.php
+++ views/home.php
@@ -10,14 +10,39 @@
     ?>
     <body>
     <br>
-        <div class="botones">         
-            <a class="btn btn-dark btn-lg" role="button" href="buscar.php">Buscar</a>
-            <a class="btn btn-dark btn-lg" role="button" href="buscarProtectoras.php">Protectoras</a>
-            <a class="btn btn-dark btn-lg" role="button" href="noticias.php">Noticias</a>
-            <a class="btn btn-dark btn-lg" role="button" href="cargar.php">
-            <img height="50px" src="../images/cargar.png">
-            </a>    
+    <br>
+    <br>
+    <br>
+            <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+            <a href="../views/buscarNoticia.php">
+                <img src="../images/imagen1.png" class="imghome" alt="Imagen 1" width="300px" >
+            </a>
+            <h2>¡Conoce las nuevas Novedades!</h2>
+            <h4>Ponete al dia con nuestras noticias de último momento.</h4>
+            </div>
+
+            <div class="col-md-4">
+            <a href="../views/buscar.php">
+                <img src="../images/imagen2.png" class="imghome" alt="Imagen 2" width="300px" >
+            </a>
+            <h2>¡Busca aquí!</h2>
+            <h4>Buscar animales, protectoras, veterinarias, etc.</h4>
+            </div>
+
+            <div class="col-md-4">
+            <a href="../views/cargar.php">
+                <img src="../images/imagen3.png" class="imghome" alt="Imagen 3" width="300px" >
+            </a>
+            <h2>¡Cargue info aquí!</h2>
+            <h4>Cargar nueva información al sistema.</h4>
+            </div>
         </div>
+        </div>
+        <br>
+        <br>
+        <br>
         <br>
         <footer>
             <?php
