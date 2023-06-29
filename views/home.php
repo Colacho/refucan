e53f6a9d411cc80f77925b323e3f41f5686ab866
geronimoc17 <gerocepeda@gmail.com>
2023-06-29 13:20:58 -0300

    apartado de donaciones

--- views/home.php
+++ views/home.php
@@ -43,6 +43,28 @@
         <br>
         <br>
         <br>
+        <div class="containerdonaciones">
+        <a href="https://link.mercadopago.com.ar/refucan" target="_blank">
+        <br>
+        <div class="row">
+        <br>
+            <div class="col-md-4">
+            <a href="https://link.mercadopago.com.ar/refucan">
+                <img src="../images/donaciones.png" class="imgdonacion" alt="Imagen 1" width="300px" >
+            </a>
+            </div>
+
+            <div class="col-md-8">
+            <h1 class="textdonacion">SE ACEPTAN DONATIVOS</h2>
+            <br>
+            <h4 class="textdonacion">Cualquier ayuda será bien recibida, los fondos que se recauden serán utilizados en el mantenimiento de nuestra Web, en proporcionarle vacunas y cuidados a todos los animales que tengamos bajo custodia y sobre todo en alimentarlos y darles una mejor calidad de vida.</h4>
+            </div>
+        </div>
+</a>
+</div>
+        <br>
+        <br>
+        <br>
         <br>
         <footer>
             <?php
