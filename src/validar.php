01f1419ecd6d018e29e4a3f91ad4914bf9cbfc26
Colacho <63317365+Colacho@users.noreply.github.com>
2023-07-05 16:24:01 -0300

    corregido validacion

--- src/validar.php
+++ src/validar.php
@@ -3,7 +3,7 @@ $input_usuario=$_POST['usuario'];
 $input_contraseña=$_POST['password'];
 
 $conexion=mysqli_connect("localhost","root","","refucan");
-$consulta="SELECT*FROM usuarios where email='$input_usuario' and pass='$input_contraseña'";
+$consulta="SELECT*FROM usuarios where correo='$input_usuario' and pass='$input_contraseña'";
 $resultado=mysqli_query($conexion,$consulta);
 $filas=mysqli_fetch_array($resultado);
 
@@ -13,11 +13,11 @@ if($resultado->num_rows > 0){
     $_SESSION['usuario']=$filas['nombre'];
     $rol = $filas['cargo_id'];
 
-    $consulta2="SELECT rol FROM cargo where cargo_id='$rol'";
+    $consulta2="SELECT nombre FROM cargos where nombre='$rol'";
     $resultado2=mysqli_query($conexion,$consulta2);
     $filas2=mysqli_fetch_array($resultado2);
 
-    $_SESSION['cargo']=$filas2['rol'];
+    $_SESSION['cargo']=$filas2['nombre'];
 
     header("location:../views/home.php");
 
